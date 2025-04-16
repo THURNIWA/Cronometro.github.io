96f32889e1a0022773b17f2df786732300ebf40b
Niwa <arthurniwa94@gmail.com>
2025-04-16 18:38:49 -0300

    Adicionando um Sleep para mostrar os numeros devagar

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using  System.Threading;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine("" + currentTime);
+                Thread.Sleep(1000);            
+            }
+        }
+    }   
+}

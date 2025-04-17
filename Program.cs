cf5b3d79f746c3b4d216c1368d56f05d55228f5d
Niwa <arthurniwa94@gmail.com>
2025-04-16 21:42:00 -0300

    Aplicação finalizada

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Cronometro
     {
         static void Main(string[] args)
         {
-            Start(6);
+            Menu();
 
         }
 
@@ -23,8 +23,33 @@ namespace Cronometro
 
             char type = char.Parse(data.Substring(data.Length -1 ,1 ));
             int time = int.Parse(data.Substring(0, data.Length -1));
+            int mult = 1;
+
+            if (type == 'm')
+            {
+                mult = 60;
+            }
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            PreStart (time * mult);
+
         }
 
+            static void PreStart(int time)
+            {
+                Console.Clear();
+                Console.WriteLine("Read....");
+                Thread.Sleep(1000);
+                Console.WriteLine("Set...");
+                Thread.Sleep(1000);
+                Console.WriteLine("Go..");
+                Thread.Sleep(2500);
+
+                Start(time);
+            }
         static void Start(int time)
         {
             int currentTime = 0;
@@ -33,12 +58,13 @@ namespace Cronometro
             {
                 Console.Clear();
                 currentTime++;
-                Console.WriteLine("" + currentTime);
+                Console.WriteLine(currentTime);
                 Thread.Sleep(1000);            
             }
             Console.Clear();
             Console.WriteLine("Cronometro finalizado!");
             Thread.Sleep(2500);
+            Menu();
         }
     }   
 }
